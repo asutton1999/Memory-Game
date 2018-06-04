7c846f057693e3eafd44cf6c7f7c4884b647d017
Angela-Sutton <angela.sutton1999@gmail.com>
2018-06-04 17:04:48 -0400

    Created event listners to each div and  stored them in variables

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
-$('.card').on('click', function showImage (event){
-$(event.target).children().css("display","block");
-console.log ("done"); })
+
+const heart = $('#heart').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const smile = $('#smile').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+ const balloon= $('#balloon').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+ const boardGame= $('#board-game').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+ const rocketShip = $('#rocket-ship').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const boat = $('#boat').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+ const car = $('#car').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const plane= $('#plane').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const heart2 = $('#heart2').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const smile2 = $('#smile2').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const balloon2 = $('#balloon2').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const boardGame2 = $('#board-game2').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const rocketShip2 = ('#rocket-ship2').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+ const boat2 = $('#boat2').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+ const car2 = $('#car2').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
+
+const plane2 = $('#plane2').on('click', function showImage (event){
+$(event.target).children().css("display","block"); } )
